16375a0b380b8c586b4f7f995fcb6b492e028aa6
Alex Carmichael <carmichael.alexander@gmail.com>
2016-02-10 17:02:32 -0800

    fixed problem where mesh was not being rendered properly

--- cloth/shaders/cloth.vs.glsl
+++ cloth/shaders/cloth.vs.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
 layout(location = 0) in vec4 vPosition;
-
+in vec3 vpoint;
 uniform mat4 Mat; //compound transform matrix
 
 void main()
 {
-	gl_Position = Mat * vPosition;
+	gl_Position = Mat * vec4(vpoint.x, vpoint.y, vpoint.z, 1.0);
 }
--- cloth/source/Cloth.cpp
+++ cloth/source/Cloth.cpp
@@ -8,13 +8,13 @@
 #include <atlas/core/Log.hpp>
 #include <math.h>
 #include <algorithm>
-
+#include <iostream>
 
 /*Ctor*/
 Cloth::Cloth() :
-mRefPosition(1, 1, 0),
+mRefPosition(0, 0, 0),
 mModelVelocity(0),
-mModelPosition(0.0f, 0.0f, 0),
+mModelPosition(-0.5f, -0.25f, 0),
 mModelOldPosition(1.0, 1.0, 0)
 {
 	USING_ATLAS_GL_NS;
@@ -22,7 +22,7 @@ mModelOldPosition(1.0, 1.0, 0)
 
 	//original position
 //	mRefMatrix = glm::translate(Matrix4(1.0f), mRefPosition);
-//	mModel = glm::translate(Matrix4(1.0f), mModelPosition);
+	mModel = glm::translate(Matrix4(1.0f), mModelPosition);
 	mModel *= glm::scale(Matrix4(1.0f), Vector(0.1f,0.1f,0.1f));
 
 	// Get the path where our shaders are stored.
@@ -47,19 +47,27 @@ mModelOldPosition(1.0, 1.0, 0)
 	GLuint mMat = mShaders[0]->getUniformVariable("Mat");
 	mUniforms.insert(UniformKey("Mat", mMat));
 
+	//--vertex array
 	glGenVertexArrays(1, &mVao);
 	glBindVertexArray(mVao);
 
-	MakeVertices(10, 10);
+	MakeVertices(12, 8);
 
+	//--buffer
 	glGenBuffers(1, &mBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, mBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vector)*triangle_vec.size(), &triangle_vec[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, (sizeof(Vector))*triangle_vec.size(), &triangle_vec[0], GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(0);
+	//using the commented out code below caused the geometry to be whacky, like not a grid
+	GLuint vpoint_id = glGetAttribLocation(mShaders[0]->getShaderProgram(), "vpoint");
+	glEnableVertexAttribArray(vpoint_id);
+	glVertexAttribPointer(vpoint_id, 3, GL_FLOAT, false, 0, 0);
+
+	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	//glEnableVertexAttribArray(0);
 
 	// Disable at the end to avoid mixing shaders.
+	glBindVertexArray(0);
 	mShaders[0]->disableShaders();
 }
 
@@ -81,16 +89,14 @@ void Cloth::renderGeometry()
 	mShaders[0]->enableShaders();
 
 	glBindVertexArray(mVao);
-
+	
 	auto mMat = mModel;
 	glUniformMatrix4fv(mUniforms["Mat"], 1, GL_FALSE, &mMat[0][0]);
 	//---draw
-	glDrawArrays(GL_LINE_STRIP, 0, triangle_vec.size());  //uncomment to see the mesh drawn as triangle strips
-
+	glDrawArrays(GL_LINES, 0, triangle_vec.size());  //uncomment to see the mesh drawn as triangle strips
 
 	// Disable them.
 	mShaders[0]->disableShaders();
-
 }
 
 void Cloth::EulerIntegrator(atlas::utils::Time const& t)
@@ -104,7 +110,7 @@ void Cloth::MakeVertices(int width, int height)
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < width; j++)
-			vertices.push_back(Vector(float(i), float(j), 0.0 ));
+			vertices.push_back(Vector(float(j), float(i), 0.0));
 	}
 
 	//triangle strip
@@ -113,9 +119,9 @@ void Cloth::MakeVertices(int width, int height)
 		//sqaures inside a triangle strip
 		for (int i = 0; i < (width - 1); ++i)
 		{
-			int topleft = j * width + i;
+			int topleft = (j+1) * width + i;
 			int topright = topleft + 1;
-			int bottomleft = (j + 1) * width + i;
+			int bottomleft = (j) * width + i;
 			int bottomright = bottomleft + 1;
 			//the upper left triangle
 			triangle_vec.push_back(vertices[topleft]);
@@ -128,8 +134,8 @@ void Cloth::MakeVertices(int width, int height)
 		}
 	}
 
-	////make the pixel coordinates for the height map texture
+	//make the pixel coordinates for the height map texture
 	//for (std::vector<Vector>::iterator itr = triangle_vec.begin(); itr != triangle_vec.end(); ++itr)
-	//	vtexcoord.push_back(Vector(*itr).x() / float(width), (*itr).z() / float(height)));
+	//	vtexcoord.push_back(Vector((*itr).x() / float(width), (*itr).z() / float(height),0.0f));
 
 }
